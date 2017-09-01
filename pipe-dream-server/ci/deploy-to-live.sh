b87538d01cff1ab4f53929167d17007de04a4544
jpw <jon@mdi.eu.com>
2017-09-01 12:25:22 +0200

    try apt-get

--- pipe-dream-server/ci/deploy-to-live.sh
+++ pipe-dream-server/ci/deploy-to-live.sh
@@ -2,6 +2,6 @@
 
 echo "client deploy to live script"
 
-sudo echo "test sudo"
-docker-compose exec
+apt-get update
+#docker-compose exec
 #cf push pipe-dream-server --docker-image jpwdocker/pipe-dream-server
