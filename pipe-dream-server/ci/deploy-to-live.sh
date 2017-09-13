2a156ff4ed50413775042925ab5a3905af20e51e
jpw <jon@mdi.eu.com>
2017-09-13 12:00:35 +0100

    deploy script now cf push-ing

--- pipe-dream-server/ci/deploy-to-live.sh
+++ pipe-dream-server/ci/deploy-to-live.sh
@@ -2,3 +2,11 @@
 
 echo "client deploy to live script"
 cf --version
+
+# log in etc. to cf
+cf api https://api.local.pcfdev.io --skip-ssl-validation
+cf auth user pass
+cf target -o pcfdev-org -s pcfdev-space
+
+# push it
+cf push pd-server -o jpwdocker/pipe-dream-server
