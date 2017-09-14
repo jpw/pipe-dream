f4079b6e6953e88370866dfbc807098497987fc1
jpw <jon@mdi.eu.com>
2017-09-14 12:03:27 +0100

    modifies deploy sh to be less app-specific

--- pipe-dream-server/ops/deploy/deploy-to-live.sh
+++ pipe-dream-server/ops/deploy/deploy-to-live.sh
@@ -8,12 +8,12 @@ cf api https://api.local.pcfdev.io --skip-ssl-validation
 cf auth user pass
 cf target -o pcfdev-org -s pcfdev-space
 
-# cd right out of app
-cd ../../../
+# cd out of ops/deploy/
+cd ../../
 
 # Push it
 # 	- push Docker Hub app
 #cf push pd-server -o jpwdocker/pipe-dream-server
 
 #	- push using CF build pack 
-cf push pd-server -f ./pipe-dream-server/manifest.yml
+cf push pd-server -f ./manifest.yml
