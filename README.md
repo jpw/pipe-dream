f9fce032deaba304ab4e7b202018b5c5aec2a428
jpw <jon@mdi.eu.com>
2020-04-01 16:24:26 +0100

    adds pm2 ecosystem file

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Pipe Dream
 
-
 A couple of apps, of no public interest, that I use to hack around with GoCD + pcf-dev + Docker integration.
+
+### To run locally
+
+Install `pm2`, then:
+
+` pm2 start ecosystem.config.js`
+
+To stop:
+
+` pm2 del ecosystem.config.js`
---
+++ ecosystem.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  apps : [{
+    name: 'server',
+    cwd: './pipe-dream-server/',
+    script: 'npm',
+    args: 'start',
+    
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '150M',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  },
+  {
+    name: 'client',
+    cwd: './pipe-dream-client/',
+    script: 'npm',
+    args: 'start',
+    
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '150M',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }
+]
+};
